a18707790330914d3165fc957d72868ef7f0ae5f
vsantos1 <vsantos1.dev@gmail.com>
2025-07-11 03:41:08 -0300

    added itch free assets
    xd

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-
-
 func _ready() -> void:
 	print("hello world")
 	
+
+func _process(delta: float) -> void:
+	print(delta)
+
+		
---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
