aab0bd103c9646e137eabad6815fa2459d80841c
vsantos1 <vsantos1.dev@gmail.com>
2025-07-21 00:19:22 -0300

    main menu scene ui and script

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends ColorRect
+
+func _on_start_pressed() -> void:
+	print("staring game")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+	
+	
