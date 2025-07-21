33fb76a538509b70d96297b4c90b6a0356a73f7d
vsantos1 <vsantos1.dev@gmail.com>
2025-07-21 03:01:59 -0300

    export tile_map to camera

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -1,24 +1,14 @@
 extends Camera2D
 
+@export var map: TileMapLayer = null
+
 func _ready() -> void:
-	var map := _find_map()
 
-	if map:
-		var map_size = map.get_used_rect()
-		var cell_size = map.tile_set.tile_size
+	var map_size = map.get_used_rect()
+	var cell_size = map.tile_set.tile_size
 
-		limit_left = map_size.position.x * cell_size.x
-		limit_top = map_size.position.y * cell_size.y
-		limit_right = (map_size.position.x + map_size.size.x) * cell_size.x
-		limit_bottom = (map_size.position.y + map_size.size.y) * cell_size.y
+	self.limit_left = map_size.position.x * cell_size.x
+	self.limit_top = map_size.position.y * cell_size.y
+	self.limit_right = (map_size.position.x + map_size.size.x) * cell_size.x
+	self.limit_bottom = (map_size.position.y + map_size.size.y) * cell_size.y
 	
-
-func _find_map() -> TileMap:
-	var current = get_parent()
-	while current:
-		if current.has_node("Map"):
-			var candidate = current.get_node("Map")
-			if candidate is TileMap:
-				return candidate
-		current = current.get_parent()
-	return null
