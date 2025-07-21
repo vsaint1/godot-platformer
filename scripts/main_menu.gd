bac3c7394fdd752e16e586a3fed0b0a93ed46d4d
vsantos1 <vsantos1.dev@gmail.com>
2025-07-21 02:16:35 -0300

    menus UI and code logic

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,6 +9,6 @@ func _on_quit_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
 	
 	
---
+++ scripts/settings_menu.gd
@@ -0,0 +1,62 @@
+extends ColorRect
+
+
+@onready var resolution_label: Label = $PanelContainer/Settings/ResolutionLabel
+@onready var resolution_option: OptionButton = $PanelContainer/Settings/ResolutionOption
+@onready var engine_slider: HSlider = $PanelContainer/Settings/EngineSlider
+@onready var vsync: CheckBox = $PanelContainer/Settings/Vsync
+
+var master_volume:float = 0.0
+
+func _ready() -> void:
+	var db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+	master_volume = db_to_linear(db)
+	engine_slider.value = master_volume
+	var vsync_mode = ProjectSettings.get_setting("display/window/vsync/vsync_mode")
+	vsync.button_pressed = vsync_mode
+	
+	if InteractionManager.is_mobile():
+		resolution_label.queue_free()
+		resolution_option.queue_free()
+
+
+func _on_return_pressed() -> void:
+	_return_to_main_menu()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_return_to_main_menu()
+
+func _return_to_main_menu() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+
+
+func _on_engine_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+
+
+func _on_resolution_option_item_selected(index: int) -> void:
+	
+	if index == 0:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		DisplayServer.window_set_size(Vector2i(1920,1080))
+	
+	if index == 1:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(Vector2i(1280,720))
+		
+	if index == 2:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(Vector2i(640,480))
+			
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+	var center_pos = (screen_size - window_size) / 2
+
+	DisplayServer.window_set_position(center_pos)
+	
+
+func _on_vsync_toggled(toggled_on: bool) -> void:
+	ProjectSettings.set_setting("display/window/vsync/vsync_mode",toggled_on)
