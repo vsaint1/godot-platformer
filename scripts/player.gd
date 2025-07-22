9655fa2c0e0a52de1ad6fa941b6e575e8e9478f2
vsantos1 <vsantos1.dev@gmail.com>
2025-07-22 19:57:12 -0300

    fix player gameplay logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPRINT_SPEED := 130.0
+const WALK_SPEED := 75.0
 
 var health := 100
 var speed := 75.0
@@ -19,15 +20,15 @@ var coin_label: Label = null
 func _ready() -> void:
 	coin_label = user_interface.get_node("CoinCount")
 	
-	if !InteractionManager.is_mobile():
-		mobile_control.queue_free()
+	_setup_mobile_gameplay()
+		
+
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
 		return
 
-	if health <= 0:
-		is_dead = true
+	if is_dead:
 		velocity = Vector2.ZERO
 		animation.play("DYING")
 		return
@@ -38,14 +39,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		jumped = true
 
-	if  jumped:
+	if  is_on_floor() && jumped:
 		velocity.y = jump_velocity
 		jumped = false
 		animation.play("JUMP")
 
-	speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else 75.0
+	speed = SPRINT_SPEED if InteractionManager.is_mobile() || Input.is_action_pressed("sprint") else WALK_SPEED
 
+	
 	var direction := Input.get_axis("move_left", "move_right")
+	
 	if direction != 0:
 		velocity.x = direction * speed
 		animation.flip_h = direction < 0
@@ -60,6 +63,17 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _setup_mobile_gameplay() -> void:
+	if !InteractionManager.is_mobile():
+		mobile_control.queue_free()
+		return
+	
 func add_coin(amount: int = 1) -> void:
 	coins += amount
 	coin_label.text = str(coins)
+
+func hurt(damage: int = 1) -> void:
+	health -= damage
+	
+	if health <=0:
+		is_dead = true
